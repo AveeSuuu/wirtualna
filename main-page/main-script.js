c0f384b54f89b8a1d9c69316e2fad1b535317ac9
Konrad Niedzielski <konrad.niedzielski03@gmail.com>
2023-12-23 17:44:53 +0100

    Zmieniłem layout górnego paska. Dodałem miejsce na dane studenta.

--- main-page/main-script.js
+++ main-page/main-script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".top").classList.add("appear");
-  
+
   const containers = document.querySelectorAll(".container-box");
 
- containers.forEach(function(container, index){
-  setTimeout(function(){
-    container.classList.add("appear");
-  }, index * 200);
- });
+  containers.forEach(function (container, index) {
+    setTimeout(function () {
+      container.classList.add("appear");
+    }, index * 200);
+  });
 });
