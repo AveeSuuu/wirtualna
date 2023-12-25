06073ba3eb65177e62ec64c9be1f891f1738d8c5
Konrad Niedzielski <konrad.niedzielski03@gmail.com>
2023-12-25 15:17:43 +0100

    Dodałem opcje z prawej. Przeniosłem logo na lewo. Poprawiłem pare rzeczy w student-box.

--- main-page/main-script.js
+++ main-page/main-script.js
@@ -2,10 +2,23 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".top").classList.add("appear");
 
   const containers = document.querySelectorAll(".container-box");
+  const moreOptions = document.querySelectorAll(".option");
 
   containers.forEach(function (container, index) {
     setTimeout(function () {
       container.classList.add("appear");
+    }, index * 100);
+  });
+
+  moreOptions.forEach(function (option, index){
+    setTimeout(function (){
+      option.classList.add("appear");
     }, index * 200);
   });
 });
+
+//TODO na następny dzień
+//1. dokończyć divy w oknie z danymi studenta
+//2. dodać im animacje pojawiania
+//3. dodać nowe komponenty z figmy
+//4. fajrant
