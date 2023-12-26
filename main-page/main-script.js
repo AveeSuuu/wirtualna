fc5a1f136c82f224c90b0611fd41c42dfb74a845
Konrad Niedzielski <konrad.niedzielski03@gmail.com>
2023-12-26 17:12:15 +0100

    dodałem animację ale strasznie laguje.

--- main-page/main-script.js
+++ main-page/main-script.js
@@ -1,33 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".top").classList.add("appear");
 
-  const containers = document.querySelectorAll(".container-box");
-  const moreOptions = document.querySelectorAll(".option");
-  const studentBoxItems = document.querySelectorAll(".box-animation");
-
+  const containers = document.querySelectorAll(".panel-option");
+  
   containers.forEach(function (container, index) {
     setTimeout(function () {
       container.classList.add("appear");
-    }, index * 100);
+    }, 100 * index);
   });
-
-  setTimeout(function(){
-    studentBoxItems.forEach(function (item, index){
-      setTimeout(function(){
+  
+  const studentBoxItems = document.querySelectorAll(".box-animation");
+  
+  setTimeout(function () {
+    studentBoxItems.forEach(function (item, index) {
+      setTimeout(function () {
         item.classList.add("appear");
-      }, index*250);
+      }, index * 250);
     })
   }, 1000);
-
-  moreOptions.forEach(function (option, index){
-    setTimeout(function (){
+  
+  const moreOptions = document.querySelectorAll(".option");
+  
+  moreOptions.forEach(function (option, index) {
+    setTimeout(function () {
       option.classList.add("appear");
     }, index * 200);
   });
 });
-
-//TODO na następny dzień
-//1. dokończyć divy w oknie z danymi studenta
-//2. dodać im animacje pojawiania
-//3. dodać nowe komponenty z figmy
-//4. fajrant
